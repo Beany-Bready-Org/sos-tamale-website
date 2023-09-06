a8dca76edd2d89db63f6662a99fba72dda765f03
the-pro7 <ameyawemmanuel900@gmail.com>
2023-09-06 21:03:10 +0000

    Changes mdade

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState, lazy } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-const About = lazy(() => import("./components/About"))
+import './App.scss'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-   <>
-   <About />
-   </>
+    <div>
+      
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="main-nav">
+        <div className='logo'>
+            
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
