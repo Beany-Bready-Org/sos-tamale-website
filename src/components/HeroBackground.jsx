d5cc2ed6e210e167b8534cf59f00447d61979953
Bean <alexanderafoko@gmail.com>
2023-09-09 16:43:21 +0000

    changes for github pages

--- src/components/HeroBackground.jsx
+++ src/components/HeroBackground.jsx
@@ -6,19 +6,28 @@ import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
+// importing background images seperately(cos using a for-loop with default js importing, causes problems with gh-pages site.)
+
+import bgImg1 from "../assets/images/bg-img1.jpg"
+import bgImg2 from "../assets/images/bg-img2.jpg"
+import bgImg3 from "../assets/images/bg-img3.jpg"
+import bgImg4 from "../assets/images/bg-img4.jpg"
+
 export default function HeroBackground({ sliderSize }) {
-  let slides = [];
-  for (let i = 1; i <= 4; i++) {
-    import(`../assets/images/bg-img1.jpg`)
-      .then((image) => {
-        slides.push(
-          <SwiperSlide key={i}>
-            <img src={image.default} alt={`Slide ${i}`} />
-          </SwiperSlide>
-        );
-      })
-      .catch((error) => console.error(error));
-  }
+  let slides = [bgImg1, bgImg2, bgImg3, bgImg4];
+
+  // cant use this for img importing:
+  // for (let i = 1; i <= 4; i++) {
+  //   import(`../assets/images/bg-img${i}.jpg`)
+  //     .then((image) => {
+  //       slides.push(
+  //         <SwiperSlide key={i}>
+  //           <img src={image.default} alt={`Slide ${i}`} />
+  //         </SwiperSlide>
+  //       );
+  //     })
+  //     .catch((error) => console.error(error));
+  // }
 
   return (
     <div className="hero__background">
@@ -31,17 +40,8 @@ export default function HeroBackground({ sliderSize }) {
           delay: 5000,
           disableOnInteraction: false,
         }}
-        modules={[Autoplay, Pagination]}
-      >
-        <SwiperSlide>
-          <p>beans</p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <p>beans</p>
-        </SwiperSlide>
-        <SwiperSlide>
-          <p>beans</p>
-        </SwiperSlide>
+        modules={[Autoplay, Pagination]}>
+        {slides}
       </Swiper>
     </div>
   );
