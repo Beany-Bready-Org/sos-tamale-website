aca435a723398665a01ece279213cc1656e22e4c
Bean <alexanderafoko@gmail.com>
2023-09-09 15:48:49 +0000

    testing more

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<Router>
+		{/* <Router> */}
 			<App />
-		</Router>
+		{/* </Router> */}
 	</React.StrictMode>
 );
