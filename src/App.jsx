53cc9b65bfa512b49cd60dda37bc0da237ca8de8
the-pro7 <ameyawemmanuel900@gmail.com>
2023-09-11 18:35:50 +0000

    resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
-
 import "./stylesheets/App.scss";
-
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import VideoSection from "./components/VideoSection";
+import DidYouKnow from "./components/DidYouKnow";
 
 export default function App() {
-  const [showNav, setShowNav] = useState(false);
+	const [showNav, setShowNav] = useState(false);
 
-  return (
-    <>
-      <header>
-        <Navbar show={showNav} setShowNav={setShowNav} />
-        <Hero />
-      </header>
+	return (
+		<>
+			<header>
+				<Navbar show={showNav} setShowNav={setShowNav} />
+				<Hero />
+			</header>
 
-      <VideoSection />
-      {/* <SuccessSection /> */}
-      <DidYouKnow />
-    </>
-  );
+			<VideoSection />
+			{/* <SuccessSection /> */}
+			<DidYouKnow />
+		</>
+	);
 }
