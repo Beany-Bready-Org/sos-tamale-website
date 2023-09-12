191d7cef2ec13aa85a9a0290cbb5263e00880380
Bean <alexanderafoko@gmail.com>
2023-09-12 14:28:53 +0000

    redo-ing info section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = ({ show, setShowNav }) => {
       <section className="secondary-nav">
         <div className="secondary-nav__text">
           <div className="secondary-nav__text__info">
-            <a href="https://www.sos-childrensvillages.org">
+            <a href="https://www.sos-childrensvillages.org" target="_blank">
               www.sos-childrensvillages.org
             </a>
             <span>059 584 4040</span>
