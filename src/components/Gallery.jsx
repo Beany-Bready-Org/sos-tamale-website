ccfec0c3876791680457384993334224fd242a5e
Bean <alexanderafoko@gmail.com>
2023-09-12 12:14:20 +0000

    almost done with gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,3 +1,5 @@
+import GalleryImage from "./GalleryImage";
+
 import "../stylesheets/Gallery.scss";
 
 import Img1 from "../assets/images/bg-img1.jpg";
@@ -5,21 +7,26 @@ import Img2 from "../assets/images/bg-img2.jpg";
 import Img3 from "../assets/images/bg-img3.jpg";
 import Img4 from "../assets/images/bg-img4.jpg";
 
-export default function Gallery() {
-  // let images = [];
-  // for (let i = 0; i < 10; i++) {
-  //   images.push(<GalleryImage />);
-  // }
-  const imageNames = [];
-  for (let i = 0; i < 4; i++) {
-    imageNames.push(`bg-img${i + 1}`);
-  }
+let images = [
+  <GalleryImage imgSrc={Img1}/>,
+  <GalleryImage imgSrc={Img3}/>,
+  <GalleryImage imgSrc={Img2}/>,
+  <GalleryImage imgSrc={Img4}/>,
+  <GalleryImage imgSrc={Img3}/>,
+  <GalleryImage imgSrc={Img2}/>,
+  <GalleryImage imgSrc={Img4}/>,
+  <GalleryImage imgSrc={Img1}/>,
+  <GalleryImage imgSrc={Img2}/>,
+  <GalleryImage imgSrc={Img1}/>,
+  <GalleryImage imgSrc={Img3}/>,
+  <GalleryImage imgSrc={Img4}/>,
+];
 
-  console.log(imageNames);
+export default function Gallery() {
   return (
     <section className="gallery">
       <div className="gallery__images">
-        <img src={Img1} alt="" />
+        {/* <img src={Img1} alt="" />
         <img src={Img2} alt="" />
         <img src={Img3} alt="" />
         <img src={Img4} alt="" />
@@ -30,7 +37,8 @@ export default function Gallery() {
         <img src={Img4} alt="" />
         <img src={Img2} alt="" />
         <img src={Img3} alt="" />
-        <img src={Img1} alt="" />
+        <img src={Img1} alt="" /> */}
+        {images}
       </div>
     </section>
   );
