13eb4e90d1039805135d0a0d965dc0fb747e2999
Bean <alexanderafoko@gmail.com>
2023-09-13 11:43:26 +0000

    .

--- src/components/CampusesSection.jsx
+++ src/components/CampusesSection.jsx
@@ -2,18 +2,25 @@ import CampusesSectionCard from "./CampusesSectionCard";
 
 import "../stylesheets/CampusesSection.scss";
 
-import AsiakwaImg from "../assets/images/asiakwa.png";
-import KumasiImg from "../assets/images/kumasi.png";
-import TemaImg from "../assets/images/tema.png";
+import TamaleImg from "../assets/images/tamale.jpg";
+import AsiakwaImg from "../assets/images/asiakwa.jpg";
+import KumasiImg from "../assets/images/kumasi.jpg";
+import TemaImg from "../assets/images/tema.jpg";
 
 export default function CampusesSection() {
   return (
     <section class="campuses-section">
       <h1 className="--header">Our Campuses</h1>
-      <p className="--description">
-        Explore Some Of Our Other Prestine Campuses.
-      </p>
+      <p className="--description">Explore Our Other Prestine Campuses.</p>
       <div className="campuses-section__items">
+        <CampusesSectionCard
+          imgSrc={TamaleImg}
+          heading="Tamale"
+          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti illum
+      ipsum, sunt officiis mollitia dolor velit dolore alias ut atque voluptatum
+      ipsa animi, consequatur recusandae."
+          direction={"row-reverse"}
+        />
         <CampusesSectionCard
           imgSrc={AsiakwaImg}
           heading="Asiakwa"
--- src/components/DidYouKnow.jsx
+++ src/components/DidYouKnow.jsx
@@ -1,30 +1,40 @@
 import React from "react";
-import didYouKnowImage from "../assets/images/didyouknow.svg"
+import didYouKnowImage from "../assets/images/didyouknow.svg";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowCircleRight,
+  faArrowUpRightFromSquare,
+} from "@fortawesome/free-solid-svg-icons";
 
 const DidYouKnow = () => {
-	return (
-		<div className="did-you-know important-section">
-			<h2 className="did-you-know__heading">Did You Know?</h2>
-			<p className="did-you-know__description">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-				sollicitudin urna non libero porta aliquet. Lorem ipsum dolor sit amet,
-				consectetur adipiscing elit.
-			</p>
-            <div className="did-you-know__buttons">
-                <Link to="#" className="--cta-secondary">Visit Our Main Website</Link>
-                <Link to="#" className="college-cta">Visit International College
-                <FontAwesomeIcon icon={faArrowCircleRight} className="arrow-icon"/>
-                </Link>
-            </div>
-            
-            <div className="overlay">
-                <img src={didYouKnowImage} alt="A background" />
-            </div>
-		</div>
-	);
+  return (
+    <div className="did-you-know important-section">
+      <h2 className="did-you-know__heading">Did You Know?</h2>
+      <p className="did-you-know__description">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+        sollicitudin urna non libero porta aliquet. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit.
+      </p>
+      <div className="did-you-know__buttons">
+        <Link to="#" className="--cta-secondary">
+          Visit Our Main Website
+          <FontAwesomeIcon
+            icon={faArrowUpRightFromSquare}
+            className="arrow-icon"
+          />
+        </Link>
+        <Link to="#" className="college-cta">
+          Visit International College
+          <FontAwesomeIcon icon={faArrowCircleRight} className="arrow-icon" />
+        </Link>
+      </div>
+
+      <div className="overlay">
+        <img src={didYouKnowImage} alt="A background" />
+      </div>
+    </div>
+  );
 };
 
 export default DidYouKnow;
