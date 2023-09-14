f9ca58db9120f739c963930c2adc624ee1f7cab9
Bean <alexanderafoko@gmail.com>
2023-09-14 10:29:35 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,52 +5,31 @@ import Hero from "./components/Hero";
 import VideoSection from "./components/VideoSection";
 import CampusesSection from "./components/CampusesSection";
 import DidYouKnow from "./components/DidYouKnow";
-<<<<<<< HEAD
 import Gallery from "./components/Gallery";
-=======
 import StaffSection from "./components/StaffSection";
 import Contact from "./components/Contact";
 import { Routes, Route } from "react-router-dom";
->>>>>>> origin/contact-page
 
 export default function App() {
-	const [showNav, setShowNav] = useState(false);
+  const [showNav, setShowNav] = useState(false);
 
-<<<<<<< HEAD
   return (
     <>
       <header>
         <Navbar show={showNav} setShowNav={setShowNav} />
         <Hero />
       </header>
-
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <main>
         <VideoSection />
         <CampusesSection />
+        <StaffSection />
         {/* <SuccessSection /> */}
         <DidYouKnow />
         <Gallery />
       </main>
     </>
   );
-=======
-	return (
-		<>
-			<header>
-				<Navbar show={showNav} setShowNav={setShowNav} />
-				<Hero />
-			</header>
-			<Routes>
-				<Route path="/contact" element={<Contact />} />
-			</Routes>
-			<main>
-				<VideoSection />
-				<CampusesSection />
-				<StaffSection />
-				{/* <SuccessSection /> */}
-				<DidYouKnow />
-			</main>
-		</>
-	);
->>>>>>> origin/contact-page
 }
