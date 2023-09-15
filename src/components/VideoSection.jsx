3d316c323e91cc9d0a93036f0ccf21fed4522c3d
the-pro7 <ameyawemmanuel900@gmail.com>
2023-09-15 17:29:27 +0000

    Trying to fix bugs

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 import IntroVideo from "./IntroVideo";
-import '../stylesheets/VideoSection.scss'
+import "../stylesheets/VideoSection.scss";
 import PlayContext from "../contexts/PlayContext";
 
 const VideoSection = () => {
-    
 	return (
 		<PlayContext>
 			<section className="video-section">
-			<h2 className="--header">Introductory Video</h2>
-			<p className="--description">
-
-				Watch the intro video below about our prestigious institution.
-			</p>
-			<IntroVideo />
-		</section>
+				<h2 className="--header">Introductory Video</h2>
+				<p className="--description">
+					Watch the intro video below about our prestigious institution.
+				</p>
+				<IntroVideo />
+			</section>
 		</PlayContext>
 	);
 };
