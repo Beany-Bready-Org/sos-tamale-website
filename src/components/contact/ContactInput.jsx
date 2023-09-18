2189cc328aa467bf0359cf34b00c5bcb1d1dc856
Bean <alexanderafoko@gmail.com>
2023-09-18 11:57:58 +0000

    updates to contact

--- src/components/contact/ContactInput.jsx
+++ src/components/contact/ContactInput.jsx
@@ -8,7 +8,8 @@ export default function ContactInput({ type, placeholder, id }) {
         name={id}
         id={id}
         key={id}
-        className="contact__form__input textarea"></textarea>
+        className="contact__form__input textarea"
+      ></textarea>
     );
   } else if (type == "submit") {
     return (
