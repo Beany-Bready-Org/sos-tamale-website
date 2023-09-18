b168a65ad233ac4b451d3033f6ef3ba3f07549cd
the-pro7 <ameyawemmanuel900@gmail.com>
2023-09-18 19:35:59 +0000

    Enroll almost complete

--- src/components/enroll/Enroll.jsx
+++ src/components/enroll/Enroll.jsx
@@ -1,31 +1,68 @@
 import React from "react";
 import sideNavImage from "../../assets/images/new-side-nav-image.svg";
 import EnrollInput from "./EnrollInput";
+import siteLogo from "../../assets/images/logo2.png";
+import "../../stylesheets/Enroll.scss";
 
 const Enroll = () => {
 	return (
-		<>
-			<form>
-				<h2 className="form-header">Enroll Your Ward</h2>
-				<p className="form-description">
+		<main className="enroll-container">
+			<form className="enroll-form">
+				<div className="enroll-form__heading">
+                <h2 className="enroll-form__heading-header">Enroll Your Ward</h2>
+				<p className="enroll-form__heading-description">
 					Please fill out each box to provide information about your ward.
 				</p>
-				<EnrollInput type="text" placeholder="Enter Your Name Here.." />
-				<EnrollInput type="email" placeholder="Enter Your Email Here.." />
-				<div className="telephone">
+                </div>
+				<EnrollInput
+					type="text"
+					placeholder="Enter Your Name Here.."
+					className="enroll-form__input"
+				/>
+				<EnrollInput
+					type="email"
+					placeholder="Enter Your Email Here.."
+					className="enroll-form__input"
+				/>
+				<div className="enroll-form__input telephone">
 					<div className="telephone__contry-code">+233</div>
 					<EnrollInput type="tel" placeholder="Enter your phone number here" />
 				</div>
-				<EnrollInput type="text" placeholder="Enter Your Ward's Name here..." />
+				<EnrollInput
+					type="text"
+					placeholder="Enter Your Ward's Name here..."
+					className="enroll-form__input"
+				/>
 				<EnrollInput
 					type="text"
 					placeholder="What's your ward's previous school...?"
+					className="enroll-form__input"
 				/>
-                <button className="--cta">
-                Make enrollment request
-                </button>
+				<button className="--cta form-cta">Make enrollment request</button>
 			</form>
-		</>
+			<header className="side-nav">
+				<nav className="secondary-navbar">
+					<div className="logo">
+						<img src={siteLogo} alt="Site logo" style={{width: "100px"}}/>
+					</div>
+					<div className="hamburger">
+						<div className="line line1"></div>
+						<div className="line line2"></div>
+						<div className="line line3"></div>
+					</div>
+				</nav>
+				<div className="side-nav__image">
+					<img src={sideNavImage} alt="Side nav image" />
+				</div>
+				<div className="side-nav__sdg">
+					<blockquote className="side-nav__sdg--four">
+						”Ensure inclusive and equitable quality education and promote
+						lifelong learning opportunities for all”
+					</blockquote>
+					<b className="side-nav__sdg--sdg">SDG 4</b>
+				</div>
+			</header>
+		</main>
 	);
 };
 
--- src/components/enroll/EnrollInput.jsx
+++ src/components/enroll/EnrollInput.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const EnrollInput = ({ type, placeholder, ref }) => {
+const EnrollInput = ({ type, placeholder, ref, className }) => {
 	switch (type) {
 		case "text":
-			return <input type={type} placeholder={placeholder} ref={ref}/>;
+			return <input type={type} placeholder={placeholder} ref={ref} className={className}/>;
 		case "email":
-			return <input type={type} placeholder={placeholder} ref={ref}/>;
+			return <input type={type} placeholder={placeholder} ref={ref} className={className}/>;
 		case "tel":
-			return <input type={type} placeholder={placeholder} ref={ref}/>;
+			return <input type={type} placeholder={placeholder} ref={ref} className={className}/>;
 		default:
-			return <input type={type} placeholder={placeholder} ref={ref}/>;
+			return <input type={type} placeholder={placeholder} ref={ref} className={className}/>;
 	}
 };
 
