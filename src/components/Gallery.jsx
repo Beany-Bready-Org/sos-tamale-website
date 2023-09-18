8b81488874e85428fcde3068a0c76124d60386c0
Bean <alexanderafoko@gmail.com>
2023-09-18 14:31:48 +0000

    .

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -17,6 +17,20 @@ import Img12 from "../assets/images/gallery-images/gallery-img12.png";
 import Img13 from "../assets/images/gallery-images/gallery-img13.png";
 import Img14 from "../assets/images/gallery-images/gallery-img14.png";
 
+import Img15 from "../assets/images/gallery-images/gallery-img15.jpg";
+import Img17 from "../assets/images/gallery-images/gallery-img17.jpg";
+import Img18 from "../assets/images/gallery-images/gallery-img18.jpg";
+import Img19 from "../assets/images/gallery-images/gallery-img19.jpg";
+
+import Img21 from "../assets/images/gallery-images/gallery-img21.jpg";
+import Img22 from "../assets/images/gallery-images/gallery-img22.jpg";
+import Img23 from "../assets/images/gallery-images/gallery-img23.jpg";
+import Img24 from "../assets/images/gallery-images/gallery-img24.jpg";
+import Img25 from "../assets/images/gallery-images/gallery-img25.jpg";
+import Img26 from "../assets/images/gallery-images/gallery-img26.jpg";
+import Img27 from "../assets/images/gallery-images/gallery-img27.jpg";
+import Img28 from "../assets/images/gallery-images/gallery-img28.jpg";
+
 let images = [
   <GalleryImage imgSrc={Img1} />,
   <GalleryImage imgSrc={Img2} />,
@@ -32,6 +46,10 @@ let images = [
   <GalleryImage imgSrc={Img12} />,
   <GalleryImage imgSrc={Img13} />,
   <GalleryImage imgSrc={Img14} />,
+  <GalleryImage imgSrc={Img15} />,
+  <GalleryImage imgSrc={Img17} />,
+  <GalleryImage imgSrc={Img18} />,
+  <GalleryImage imgSrc={Img19} />,
 ];
 
 export default function Gallery() {
