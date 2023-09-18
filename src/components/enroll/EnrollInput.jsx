fdb116d2c2c11dd433fa8314ae61d6762e4d3a8f
the-pro7 <ameyawemmanuel900@gmail.com>
2023-09-18 21:36:21 +0000

    enroll finished

--- src/components/enroll/EnrollInput.jsx
+++ src/components/enroll/EnrollInput.jsx
@@ -1,16 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const EnrollInput = ({ type, placeholder, ref, className }) => {
+const EnrollInput = ({ type, placeholder, ref, className, name }) => {
 	switch (type) {
 		case "text":
-			return <input type={type} placeholder={placeholder} ref={ref} className={className}/>;
+			return (
+				<input
+					type={type}
+					placeholder={placeholder}
+					ref={ref}
+					className={className}
+					name={name}
+				/>
+			);
 		case "email":
-			return <input type={type} placeholder={placeholder} ref={ref} className={className}/>;
+			return (
+				<input
+					type={type}
+					placeholder={placeholder}
+					ref={ref}
+					className={className}
+					name={name}
+				/>
+			);
 		case "tel":
-			return <input type={type} placeholder={placeholder} ref={ref} className={className}/>;
+			return (
+				<input
+					type={type}
+					placeholder={placeholder}
+					ref={ref}
+					className={className}
+					name={name}
+				/>
+			);
 		default:
-			return <input type={type} placeholder={placeholder} ref={ref} className={className}/>;
+			return (
+				<input
+					type={type}
+					placeholder={placeholder}
+					ref={ref}
+					className={className}
+					name={name}
+				/>
+			);
 	}
 };
 
