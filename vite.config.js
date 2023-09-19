9049f579f5159a733d7087221ef983df03ed76d2
Bean <alexanderafoko@gmail.com>
2023-09-19 13:02:07 +0000

    removed an unwanted line of code

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import replace from "@rollup/plugin-replace";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
