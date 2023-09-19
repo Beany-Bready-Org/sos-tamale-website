f242db635ca76350478affdbd22e6ea88d2c8076
Bean <alexanderafoko@gmail.com>
2023-09-19 10:20:46 +0000

    pls work pls - gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename="/sos-tamale-website">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
