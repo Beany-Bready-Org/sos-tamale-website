9bc6cfcead961949aa4797cf569043958726fcf5
the-pro7 <ameyawemmanuel900@gmail.com>
2023-09-19 14:26:12 +0000

    Problem fixed

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename="/sos-tamale-website">
+    <Router>
       <App />
     </Router>
   </React.StrictMode>
