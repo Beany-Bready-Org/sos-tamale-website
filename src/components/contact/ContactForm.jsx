e7b7859bab4bea2dd2b86ec74fa47517adb7615e
the-pro7 <ameyawemmanuel900@gmail.com>
2023-09-20 17:12:33 +0000

    changes made to contact

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,33 +1,115 @@
+import { useRef, useState } from "react";
 import "../../stylesheets/ContactForm.scss";
 import ContactInput from "./ContactInput";
+import emailjs from "@emailjs/browser";
+// import { Alert } from "react-bootstrap";
 
 export default function ContactForm() {
-  return (
-    <form className="contact__form">
-      <h1 className="--header" style={{ color: "black" }}>
-        Contact Us
-      </h1>
-      <ContactInput
-        type={"text"}
-        placeholder={"Type your full name here"}
-        id={"name"}
-      />
-      <ContactInput
-        type={"email"}
-        placeholder={"Type your email here"}
-        id={"email"}
-      />
-      <ContactInput
-        type={"text"}
-        placeholder={"Type your subject here"}
-        id={"subject"}
-      />
-      <ContactInput
-        type={"textarea"}
-        placeholder={"Type your message here..."}
-        id={"message"}
-      />
-      <ContactInput type={"submit"} placeholder={"Submit"} id={"button"} />
-    </form>
-  );
+	const [loading, setLoading] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
+	const [successMessage, setSuccessMessage] = useState("");
+
+	// Input Refs
+	const fullNameRef = useRef();
+	const emailRef = useRef();
+	const subjectRef = useRef();
+	const messageRef = useRef();
+
+	// Form Ref
+	const formRef = useRef();
+
+	// Custom messages
+	const showMessageForSomeTime = (errorMessage, successMessage, duration) => {
+		setErrorMessage(errorMessage);
+		setSuccessMessage(successMessage);
+
+		setTimeout(() => {
+			setErrorMessage("");
+			setSuccessMessage("");
+		}, duration);
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const templateId = "template_zlkgrnn";
+		const serviceId = "HxfJG_ab_z5fbm2qm";
+		const publicKey = "service_k9t86zv";
+
+		const formData = {
+			from_name: fullNameRef.current.value,
+			from_email: emailRef.current.value,
+			from_subject: subjectRef.current.value,
+			message: messageRef.current.value,
+		};
+
+		if (
+			!formData.from_name ||
+			!formData.from_email ||
+			!formData.from_subject ||
+			!formData.message
+		) {
+			showMessageForSomeTime("Cannot submit an empty form", "", 2000);
+			setLoading(false);
+			return;
+		}
+
+		try {
+			setLoading(true);
+			await emailjs.sendForm(serviceId, templateId, formRef.current, publicKey);
+			showMessageForSomeTime("", "Thank You For Reaching Out!", 2000);
+			formRef.current.reset();
+		} catch (error) {
+			showMessageForSomeTime(error, "", 2000);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	return (
+		<form className="contact__form" onSubmit={handleSubmit} ref={formRef}>
+			{errorMessage || successMessage ? (
+				<p className={errorMessage ? "error" : "success"}>
+					{errorMessage ? errorMessage : successMessage}
+				</p>
+			) : null}
+			<h1 className="--header" style={{ color: "black" }}>
+				Contact Us
+			</h1>
+			<ContactInput
+				type={"text"}
+				placeholder={"Type your full name here"}
+				id={"name"}
+				name="from_name"
+				inputRef={fullNameRef}
+			/>
+			<ContactInput
+				type={"email"}
+				placeholder={"Type your email here"}
+				id={"email"}
+				name="from_email"
+				inputRef={emailRef}
+			/>
+			<ContactInput
+				type={"text"}
+				placeholder={"Type your subject here"}
+				id={"subject"}
+				name="from_subject"
+				inputRef={subjectRef}
+			/>
+			<ContactInput
+				type={"textarea"}
+				placeholder={"Type your message here..."}
+				id={"message"}
+				name="message"
+				inputRef={messageRef}
+			/>
+			<ContactInput
+				type={"submit"}
+				placeholder={loading ? "Submitting..." : "Submit"}
+				id={"button"}
+				loading={loading}
+			/>
+		</form>
+	);
 }
--- src/components/contact/ContactInput.jsx
+++ src/components/contact/ContactInput.jsx
@@ -1,37 +1,57 @@
 import "../../stylesheets/ContactInput.scss";
+import PropTypes from "prop-types";
 
-export default function ContactInput({ type, placeholder, id }) {
-  if (type == "textarea") {
-    return (
-      <textarea
-        placeholder={placeholder}
-        name={id}
-        id={id}
-        key={id}
-        className="contact__form__input textarea --input"
-      ></textarea>
-    );
-  } else if (type == "submit") {
-    return (
-      <input
-        type={type}
-        value={placeholder}
-        id={id}
-        name={id}
-        key={id}
-        className="contact__form__input --cta --input"
-      />
-    );
-  } else {
-    return (
-      <input
-        type={type}
-        placeholder={placeholder}
-        id={id}
-        name={id}
-        key={id}
-        className="contact__form__input --input"
-      />
-    );
-  }
+export default function ContactInput({
+	type,
+	placeholder,
+	id,
+	inputRef,
+	nameValue,
+	loading,
+}) {
+	if (type == "textarea") {
+		return (
+			<textarea
+				placeholder={placeholder}
+				name={id}
+				id={nameValue}
+				key={id}
+				ref={inputRef}
+				className="contact__form__input textarea --input"
+			></textarea>
+		);
+	} else if (type == "submit") {
+		return (
+			<input
+				type={type}
+				value={placeholder}
+				id={id}
+				name={nameValue}
+				key={id}
+				ref={inputRef}
+				className="contact__form__input --cta --input"
+				disabled={loading}
+				style={loading ? {boxShadow: "none", backgroundColor: "#ced4da" } : null}
+			/>
+		);
+	} else {
+		return (
+			<input
+				type={type}
+				placeholder={placeholder}
+				id={id}
+				name={nameValue}
+				key={id}
+				ref={inputRef}
+				className="contact__form__input --input"
+			/>
+		);
+	}
 }
+
+ContactInput.propTypes = {
+	id: PropTypes.string.isRequired,
+	type: PropTypes.string.isRequired,
+	placeholder: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired
+};
