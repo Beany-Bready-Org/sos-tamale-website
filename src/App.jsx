b2b9fc9390837e2d7e25811120ddbc53e6ee80cc
Felix407 <59978286+Felix407@users.noreply.github.com>
2023-09-25 14:51:01 +0100

    image loading updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import React, { lazy } from "react";
 import "./stylesheets/App.scss";
 import { Routes, Route, redirect } from "react-router-dom";
 import Home from "./components/Home";
-// import Contact from "./components/contact/Contact";
-// import About from "./components/About";
-// import Gallery from "./components/gallery/Gallery";
-// import Enroll from "./components/enroll/Enroll";
+import Contact from "./components/contact/Contact";
+import About from "./components/About";
+import Gallery from "./components/gallery/Gallery";
+import Enroll from "./components/enroll/Enroll";
 import NavContextProvider from "./contexts/NavContext";
 import Navbar from "./components/Navbar";
-// import RulesRegulations from "./components/rnr/RulesRegulations";
-// Lazily loaded components
-const Contact = lazy(() => import( "./components/contact/Contact"));
-const About = lazy(() => import("./components/About"));
-const Gallery = lazy(() => import("./components/gallery/Gallery"))
-const Enroll = lazy(() => import("./components/enroll/Enroll"))
-const RulesRegulations = lazy(() => import("./components/rnr/RulesRegulations"))
+import RulesRegulations from "./components/rnr/RulesRegulations";
+// Lazily loaded components coming soon
+// const Contact = lazy(() => import( "./components/contact/Contact"));
+// const About = lazy(() => import("./components/About"));
+// const Gallery = lazy(() => import("./components/gallery/Gallery"))
+// const Enroll = lazy(() => import("./components/enroll/Enroll"))
+// const RulesRegulations = lazy(() => import("./components/rnr/RulesRegulations"))
 
 
 export default function App() {
