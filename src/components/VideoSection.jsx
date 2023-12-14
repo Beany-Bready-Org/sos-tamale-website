38a1c44e3d505551171ed5db6ae90aa64074c227
the-pro7 <ameyawemmanuel900@gmail.com>
2023-12-14 20:49:52 +0000

    Almost done

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -2,22 +2,25 @@ import IntroVideo from "./IntroVideo";
 import "../stylesheets/VideoSection.scss";
 import PlayContext from "../contexts/PlayContext";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const VideoSection = () => {
-  return (
-    <PlayContext>
-      <section className="video-section">
-        <h2 className="--header">Introductory Video</h2>
-        <p className="--description">
-          Watch the intro video below about our prestigious institution.
-        </p>
-        <IntroVideo />
-        <Link to="/about" className="--cta">
-          Learn more about us
-        </Link>
-      </section>
-    </PlayContext>
-  );
+	const [loopVideo, setLoopVideo] = useState(false);
+	const [autoplayVideo, setAutoPlayVideo] = useState(false);
+	return (
+		<PlayContext>
+			<section className="video-section">
+				<h2 className="--header">Introductory Video</h2>
+				<p className="--description">
+					Watch the intro video below about our prestigious institution.
+				</p>
+				<IntroVideo loop={loopVideo} autoplay={autoplayVideo} />
+				<Link to="/about" className="--cta">
+					Learn more about us
+				</Link>
+			</section>
+		</PlayContext>
+	);
 };
 
 export default VideoSection;
