9c295490c2cd33c161c81550f14855f2362d696e
the-pro7 <ameyawemmanuel900@gmail.com>
2024-01-25 19:01:26 +0000

    env CHANGES

--- src/components/enroll/Enroll.jsx
+++ src/components/enroll/Enroll.jsx
@@ -43,7 +43,7 @@ const Enroll = () => {
 		e.preventDefault();
 		setErrorMessage("");
 
-		const serviceId = "service_zwdqe87";
+		const serviceId = "service_zwdqe87" || process.env.SERVICE_ID_ENROLL;
 		const templateId = "template_ni7p3hr";
 		const publicKey = "HxfJG_ab_z5fbm2qm";
 
