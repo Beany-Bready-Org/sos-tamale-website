7579f4f9d33f0c6bd3b606aa7e5c7bae3c0fab3d
the-pro7 <ameyawemmanuel900@gmail.com>
2024-02-16 18:24:35 +0000

    Update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const mongoose = require("mongoose");
 const upload = require("express-fileupload");
 const path = require("path");
 // Configure dotenv
 dotenv.config();
+const establishDbConnection = require("./utils/connectDB.js")
 
 const app = express();
 
@@ -28,13 +28,5 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const adminRoutes = require("./routes/adminRoutes.js");
 app.use("/api/admin", adminRoutes);
 
-// Start mongoose database connection
-mongoose.connect(process.env.CONNECTION_STRING).then(() => {
-	app.listen(port, () => console.log(`Server running on ${port}`));
-});
-
-const connection = mongoose.connection;
-
-connection.once("open", () => {
-	console.log("MongoDB just the fun!!!!");
-});
+// ESatablish MongoDB database connection
+establishDbConnection(process.env.CONNECTION_STRING, port, app)
