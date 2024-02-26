da1ced6d3adb879e96d4364895d163ff4186e1a6
the-pro7 <ameyawemmanuel900@gmail.com>
2024-02-26 08:14:09 -0800

    Update register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,8 +11,10 @@ import Navbar from "./components/Navbar";
 import RulesRegulations from "./components/rnr/RulesRegulations";
 import Events from "./components/events-page/Events";
 import Register from "./components/accounts/Register";
+// Contexts
 import AccessBoxProvider from "./contexts/AccessBoxContext";
 import AccessTokenBox from "./components/accounts/AccessTokenBox";
+import StatusMessageContext from "./contexts/StatusMessageContext";
 import Dashboard from "./components/admin-dashboard/Dashboard";
 import ProtectedRoutes from "./protected-routes/ProtectedRoutes";
 
@@ -21,39 +23,41 @@ export default function App() {
   return (
     <>
       <NavContextProvider>
-        <AccessBoxProvider>
-          <Navbar />
-          <AccessTokenBox />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/enroll" element={<Enroll />} />
-            <Route path="/gallery" element={<Gallery />} />
-            <Route
-              path="/rules-and-regulations"
-              element={<RulesRegulations />}
-            />
-            <Route path="/events" element={<Events />} />
-            {/* Protected routes => accessToken required */}
-            <Route
-              path="/register"
-              element={
-                <ProtectedRoutes>
-                  <Register />
-                </ProtectedRoutes>
-              }
-            />
-            <Route
-              path="/admin-dashboard"
-              element={
-                <ProtectedRoutes>
-                  <Dashboard />
-                </ProtectedRoutes>
-              }
-            />
-          </Routes>
-        </AccessBoxProvider>
+        <StatusMessageContext>
+          <AccessBoxProvider>
+            <Navbar />
+            <AccessTokenBox />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/enroll" element={<Enroll />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route
+                path="/rules-and-regulations"
+                element={<RulesRegulations />}
+              />
+              <Route path="/events" element={<Events />} />
+              {/* Protected routes => accessToken required */}
+              <Route
+                path="/register"
+                element={
+                  <ProtectedRoutes>
+                    <Register />
+                  </ProtectedRoutes>
+                }
+              />
+              <Route
+                path="/admin-dashboard"
+                element={
+                  <ProtectedRoutes>
+                    <Dashboard />
+                  </ProtectedRoutes>
+                }
+              />
+            </Routes>
+          </AccessBoxProvider>
+        </StatusMessageContext>
       </NavContextProvider>
     </>
   );
--- client/src/components/accounts/Register.jsx
+++ client/src/components/accounts/Register.jsx
@@ -1,64 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
 import RegisterInput from "./RegisterInput";
 import sideImage from "../../assets/images/bg-img4.jpg";
 import { useNavigate } from "react-router-dom";
 
 import "../../stylesheets/Register.scss";
+import { useStatusMessage } from "../../contexts/StatusMessageContext";
 
 export default function Register() {
-  const navigate = useNavigate()
+  // Retrieve accessToken from localStorage
+  const accessToken = localStorage.getItem("accessToken");
+  // Navigate hook
+  const navigate = useNavigate();
 
-	return (
-		<main className="register">
+//  const { statusMessage } = useStatusMessage()
+
+  //   Admin credentials state
+  const [adminCredentials, setAdminCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    accessToken: "",
+  });
+
+  //   Function to handle credentials state
+  const handleCredentialsUpdate = (e) => {
+    setAdminCredentials((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  // Function to perform admin sign up
+  const handleRegistration = async (e) => {
+    e.preventDefault();
+
+    const postOptions = {
+      method: "POST",
+      "Content-Type": "application/json",
+      body: JSON.stringify({ ...adminCredentials, accessToken }),
+    };
+
+    try {
+      const response = await fetch("http://localhost:5001/api/admin/register");
+
+	  if(!response.ok) {
+		throw new Error("Failed to register" + response.statusText)
+	  }
+
+	  let data = response.json()
+
+	  let {message} = data
+	  console.log(message)
+	  navigate("/admin-dashboard")
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // console.log(adminCredentials)
+
+  return (
+    <main className="register">
       <div className="register__image">
-      <div className="register__image__notice">
-      <h1>Admin Account</h1>
-        <p>Register as an admin to overlook events that transpire on this website.</p>
+        <div className="register__image__notice">
+          <h1>Admin Account</h1>
+          <p>
+            Register as an admin to overlook events that transpire on this
+            website.
+          </p>
+        </div>
+        <img src={sideImage} alt="" className="register__image__item" />
       </div>
-			<img src={sideImage} alt="" className="register__image__item" />
+      <form
+        className="register__form"
+        // onSubmit={handleSubmit}
+        // ref={formRef}
+      >
+        <div className="register__form__heading">
+          <h2 className="register__form__heading__header">
+            Register as an Admin
+          </h2>
+        </div>
 
-      </div>
-			<form
-				className="register__form"
-				// onSubmit={handleSubmit}
-				// ref={formRef}
-			>
-				<div className="register__form__heading">
-					<h2 className="register__form__heading__header">
-						Register as an Admin
-					</h2>
-				</div>
+        <RegisterInput
+          type="name"
+          placeholder="Enter Your Name"
+          nameValue="name"
+          onChangeHandler={handleCredentialsUpdate}
+        />
 
-				<RegisterInput
-					type="name"
-					placeholder="Enter Your Name"
-					// inputRef={emailRef}
-					nameValue="formName"
-				/>
+        <RegisterInput
+          type="email"
+          placeholder="Enter Your Email"
+          nameValue="email"
+          onChangeHandler={handleCredentialsUpdate}
+        />
 
-				<RegisterInput
-					type="email"
-					placeholder="Enter Your Email"
-					// inputRef={emailRef}
-					nameValue="formEmail"
-				/>
+        <RegisterInput
+          type="password"
+          placeholder="Enter Your Password"
+          nameValue="password"
+          onChangeHandler={handleCredentialsUpdate}
+        />
 
-				<RegisterInput
-					type="password"
-					placeholder="Enter Your Password"
-					// inputRef={emailRef}
-					nameValue="formPassword"
-				/>
-				{/* 
-				<RegisterInput
-					type="password"
-					placeholder="Enter Your Password"
-					// inputRef={emailRef}
-					nameValue="formPassword"
-				/> */}
-				<button className="register-cta --cta" type="submit">Submit</button>
-        <button type="button" className="cancel-cta --danger" onClick={() => navigate(-1)}>Cancel</button>
-			</form>
-		</main>
-	);
+        <RegisterInput
+          type="password"
+          placeholder="Confirm Your Password"
+          nameValue="confirmPassword"
+          onChangeHandler={handleCredentialsUpdate}
+        />
+        <button className="register-cta --cta" type="submit">
+          Register
+        </button>
+        <button
+          type="button"
+          className="cancel-cta --danger"
+          onClick={() => navigate(-1)}
+        >
+          Cancel
+        </button>
+      </form>
+    </main>
+  );
 }
