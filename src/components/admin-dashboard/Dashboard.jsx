af7e3ccb7aa0082ccd69c39ee9dcdf16c0b29096
Bean <alexanderafoko@gmail.com>
2024-02-26 13:40:45 +0000

    changes made for dashboard

--- src/components/admin-dashboard/Dashboard.jsx
+++ src/components/admin-dashboard/Dashboard.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 
-const Dashboard = () => {
+export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <main className="dashboard">
+      <div className="dashboard__header">
+        <h1>Welcome, Admin</h1>
+      </div>
+      <div className="dashboard__main">
+        <div className="dashboard__main__events">
+          <div className="dashboard__main__events__header">
+            <h1>
+              Manage Events{" "}
+              <FontAwesomeIcon icon={faArrowCircleRight} />
+            </h1>
+          </div>
+          <div className="dashboard__main__events__items">
+            
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
-
-export default Dashboard
--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -10,120 +10,120 @@ import emailjs from "@emailjs/browser";
 // import { Alert } from "react-bootstrap";
 
 export default function ContactForm() {
-	const [loading, setLoading] = useState(false);
-	const [errorMessage, setErrorMessage] = useState("");
-	const [successMessage, setSuccessMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
-	// Input Refs
-	const fullNameRef = useRef();
-	const emailRef = useRef();
-	const subjectRef = useRef();
-	const messageRef = useRef();
+  // Input Refs
+  const fullNameRef = useRef();
+  const emailRef = useRef();
+  const subjectRef = useRef();
+  const messageRef = useRef();
 
-	// Form Ref
-	const formRef = useRef();
+  // Form Ref
+  const formRef = useRef();
 
-	// Custom messages
-	const showMessageForSomeTime = (errorMessage, successMessage, duration) => {
-		setErrorMessage(errorMessage);
-		setSuccessMessage(successMessage);
+  // Custom messages
+  const showMessageForSomeTime = (errorMessage, successMessage, duration) => {
+    setErrorMessage(errorMessage);
+    setSuccessMessage(successMessage);
 
-		setTimeout(() => {
-			setErrorMessage("");
-			setSuccessMessage("");
-		}, duration);
-	};
+    setTimeout(() => {
+      setErrorMessage("");
+      setSuccessMessage("");
+    }, duration);
+  };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-		const templateId = "template_zlkgrnn";
-		const serviceId = "service_k9t86zv";
-		const publicKey = "HxfJG_ab_z5fbm2qm";
+    const templateId = "template_zlkgrnn";
+    const serviceId = "service_k9t86zv";
+    const publicKey = "HxfJG_ab_z5fbm2qm";
 
-		const formData = {
-			from_name: fullNameRef.current.value,
-			from_email: emailRef.current.value,
-			from_subject: subjectRef.current.value,
-			message: messageRef.current.value,
-		};
+    const formData = {
+      from_name: fullNameRef.current.value,
+      from_email: emailRef.current.value,
+      from_subject: subjectRef.current.value,
+      message: messageRef.current.value,
+    };
 
-		if (
-			!formData.from_name ||
-			!formData.from_email ||
-			!formData.from_subject ||
-			!formData.message
-		) {
-			showMessageForSomeTime("Cannot submit an empty form", "", 2000);
-			setLoading(false);
-			return;
-		}
+    if (
+      !formData.from_name ||
+      !formData.from_email ||
+      !formData.from_subject ||
+      !formData.message
+    ) {
+      showMessageForSomeTime("Cannot submit an empty form", "", 2000);
+      setLoading(false);
+      return;
+    }
 
-		try {
-			setLoading(true);
-			await emailjs.sendForm(serviceId, templateId, formRef.current, publicKey);
-			showMessageForSomeTime("", "Thank You For Reaching Out!", 2000);
-			formRef.current.reset();
-		} catch (error) {
-			showMessageForSomeTime(error, "", 2000);
-		} finally {
-			setLoading(false);
-		}
-	};
+    try {
+      setLoading(true);
+      await emailjs.sendForm(serviceId, templateId, formRef.current, publicKey);
+      showMessageForSomeTime("", "Thank You For Reaching Out!", 2000);
+      formRef.current.reset();
+    } catch (error) {
+      showMessageForSomeTime(error, "", 2000);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-	return (
-		<form className="contact__form" onSubmit={handleSubmit} ref={formRef}>
-			{errorMessage || successMessage ? (
-				<p className={errorMessage ? "error" : "success"}>
-					{errorMessage ? errorMessage : successMessage}
-				</p>
-			) : null}
-			<div
-				style={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-				}}
-			>
-				<h1 className="--header contact-header" style={{ color: "black" }}>
-					Contact Us
-				</h1>
-				<p className="contact-des">Contact us now for further enquiries.</p>
-			</div>
-			<ContactInput
-				type={"text"}
-				placeholder={"Type your full name here"}
-				id={"name"}
-				nameValue="from_name"
-				inputRef={fullNameRef}
-			/>
-			<ContactInput
-				type={"email"}
-				placeholder={"Type your email here"}
-				id={"email"}
-				nameValue="from_email"
-				inputRef={emailRef}
-			/>
-			<ContactInput
-				type={"text"}
-				placeholder={"Type your subject here"}
-				id={"subject"}
-				nameValue="from_subject"
-				inputRef={subjectRef}
-			/>
-			<ContactInput
-				type={"textarea"}
-				placeholder={"Type your message here..."}
-				id={"message"}
-				nameValue="message"
-				inputRef={messageRef}
-			/>
-			<ContactInput
-				type={"submit"}
-				placeholder={loading ? "Submitting..." : "Submit"}
-				id={"button"}
-				loading={loading}
-			/>
-		</form>
-	);
+  return (
+    <form className="contact__form" onSubmit={handleSubmit} ref={formRef}>
+      {errorMessage || successMessage ? (
+        <p className={errorMessage ? "error" : "success"}>
+          {errorMessage ? errorMessage : successMessage}
+        </p>
+      ) : null}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <h1 className="--header" style={{ color: "black" }}>
+          Contact Us
+        </h1>
+        <p className="	--description">Contact us now for further enquiries.</p>
+      </div>
+      <ContactInput
+        type={"text"}
+        placeholder={"Type your full name here"}
+        id={"name"}
+        nameValue="from_name"
+        inputRef={fullNameRef}
+      />
+      <ContactInput
+        type={"email"}
+        placeholder={"Type your email here"}
+        id={"email"}
+        nameValue="from_email"
+        inputRef={emailRef}
+      />
+      <ContactInput
+        type={"text"}
+        placeholder={"Type your subject here"}
+        id={"subject"}
+        nameValue="from_subject"
+        inputRef={subjectRef}
+      />
+      <ContactInput
+        type={"textarea"}
+        placeholder={"Type your message here..."}
+        id={"message"}
+        nameValue="message"
+        inputRef={messageRef}
+      />
+      <ContactInput
+        type={"submit"}
+        placeholder={loading ? "Submitting..." : "Submit"}
+        id={"button"}
+        loading={loading}
+      />
+    </form>
+  );
 }
