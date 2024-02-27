2dd22430c01587b603a843174327715205b26714
Bean <alexanderafoko@gmail.com>
2024-02-27 13:13:25 +0000

    g

--- client/src/protected-routes/ProtectedRoutes.jsx
+++ client/src/protected-routes/ProtectedRoutes.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const ProtectedRoutes = ({ children }) => {
-  const accessToken = localStorage.getItem("accessToken") || "";
+  // const accessToken = localStorage.getItem("accessToken") || "";
+  const accessToken = "kNZep&oE64"
 
   const navigate = useNavigate();
 
